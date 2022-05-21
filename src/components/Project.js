05ab195a58ffa8ac77d21ca2e24a02b9b8388a3a
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-05-20 18:57:33 -0700

    added flutter icon, moved projects to separate file, added project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,9 @@
-import { Avatar, Card, CardActionArea, CardActions, CardContent, Chip, Collapse, Divider, Typography, Grid } from '@mui/material';
+import { Android, Api, Css, GitHub, Html, Javascript, NavigateBefore, NavigateNext, Web } from '@mui/icons-material';
+import { Backdrop, Box, Card, CardActionArea, CardActions, CardContent, Chip, Collapse, Divider, Grid, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Web, Javascript, GitHub, Android, Api } from '@mui/icons-material';
 import React from 'react';
+import Header from './Header.js';
+import NavigateButton from './NavigationButton.js';
 import { SocialButton } from './SocialButton.js';
 
 const ACTION_ICONS = {
@@ -25,15 +27,37 @@ const CHIP_DATA = {
         icon: "icons/Firebase Hosting (1- Icon, Light).svg",
         color: "Firebase"
     },
+    "Java": {
+        url: "https://www.java.com/en/",
+        icon: "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png"
+    },
+    "HTML": {
+        icon: <Html color="HTML" />,
+        color: "HTML"
+    },
+    "CSS": {
+        icon: <Css color="CSS" />,
+        color: "CSS"
+    },
     "JavaScript": {
         url: "https://www.javascript.com/",
         icon: <Javascript color="JavaScript"/>,
         color: "JavaScript"
     },
+    "Flutter": {
+        url: "https://flutter.dev/",
+        icon: "icons/Flutter.svg",
+        color: "Flutter"
+    },
     "React": {
         url: "https://reactjs.org/",
         icon: "icons/React.svg",
         color: "React"
+    },
+    "Android": {
+        url: "https://developer.android.com/",
+        icon: <Android color="Android" />,
+        color: "Android"
     },
     "Android Studio": {
         url: "https://developer.android.com/",
@@ -70,11 +94,29 @@ const CHIP_THEME = createTheme({
             dark: "#039be5",
             contrastText: "#f57c00"
         },
+        HTML: {
+            light: "#f16524",
+            main: "#f16524",
+            dark: "#f16524",
+            contrastText: "#ffffff"
+        },
+        CSS: {
+            light: "#2965f1",
+            main: "#2965f1",
+            dark: "#2965f1",
+            contrastText: "#ffffff"
+        },
         JavaScript: {
             light: "#fcdc00",
             main: "#fcdc00",
             dark: "#fcdc00",
             contrastText: "#181818"
+        },
+        Flutter: {
+            light: "#61c9f9",
+            main: "#61c9f9",
+            dark: "#61c9f9",
+            contrastText: "#ffffff"
         },
         React: {
             light: "#282c34",
@@ -103,12 +145,40 @@ const CHIP_THEME = createTheme({
     }
 });
 
-class Project extends React.Component {
-    render() {
-        return (
-            <p>Hello!</p>
-        )
-    }
+function Project(props) {
+    // const navigate = useNavigate();
+    // const location = useLocation();
+    // const navigationType = useNavigationType();
+    // console.log(navigationType);
+
+    return (
+        <Box>
+            <Header />
+            <Box sx={{ p: 2, pb: 10 }}>
+                <p>WIP</p>
+            </Box>
+            {props.prev ?
+                <NavigateButton href={`/projects/${props.prev.title.replaceAll(" ", "%20")}`} left='16px'>
+                    <NavigateBefore />
+                    {props.prev.title}
+                </NavigateButton> : null
+            }
+            {props.next ?
+                <NavigateButton href={`/projects/${props.next.title.replaceAll(" ", "%20")}`} right='16px'>
+                    {props.next.title}
+                    <NavigateNext />
+                </NavigateButton> : null
+            }
+        </Box>
+    )
+}
+
+function ProjectDialog(props) {
+    // console.log("Render dialog");
+    // console.log(props);
+    <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={Object.keys(props).length > 0}>
+        <Project {...props} />
+    </Backdrop>
 }
 
 function ProjectCard(props) {
@@ -127,19 +197,19 @@ function ProjectCard(props) {
          * Add filter tab here. Use grid layout (?)
          */
         <Card raised={expanded} sx={{ position: 'relative', width: 350, minHeight: 500 }} onMouseEnter={handleExpand} onMouseLeave={handleContract}>
-            <CardActionArea sx={{ minHeight: "100%" }}>
+            <CardActionArea sx={{ minHeight: "100%" }} onClick={props.navigate}>
                 <CardContent>
                     <Typography variant="h5">
-                        {props.details.title}
+                        {props.title}
                     </Typography>
                     <Typography color="text.secondary">
-                        {props.details.type}
+                        {props.type}
                     </Typography>
                     <Typography variant="subtitle" color="text.secondary" gutterBottom>
-                        {props.details.date}
+                        {props.date}
                     </Typography>
                     <Typography variant="body2" sx={{ mt: 1 }} >
-                        {props.details.shortDescription}
+                        {props.shortDescription}
                     </Typography>
                 </CardContent>
             </CardActionArea>
@@ -151,14 +221,14 @@ function ProjectCard(props) {
                     </Typography>
                     <Grid container direction="row" spacing={1}>
                         <ThemeProvider theme={CHIP_THEME}>
-                            {props.details.techStack.map((element, index) => {
+                            {props.techStack.map((element, index) => {
                                 const metadata = CHIP_DATA[element];
-                                const avatar = metadata?.icon && <Avatar alt={element} src={typeof (metadata.icon) === "string" ? metadata.icon : ""} sx={{ bgcolor: "transparent" }}>{metadata.icon}</Avatar>
+                                // const avatar = metadata?.icon && <Avatar alt={element} src={typeof (metadata.icon) === "string" ? metadata.icon : ""} sx={{ bgcolor: "transparent" }}>{metadata.icon}</Avatar>
                                 return <Grid item key={index}>
                                     <Chip
                                         key={index}
                                         label={element}
-                                        avatar={avatar}
+                                        // avatar={avatar}
                                         component={metadata && "a"}
                                         href={metadata?.url}
                                         clickable={metadata?.url !== undefined}
@@ -173,11 +243,12 @@ function ProjectCard(props) {
                         <Chip label="School" /> */}
                 </CardContent>
             </Collapse>
-            <CardActions sx={{ position: 'absolute', bottom: 0 }}>
-                {props.details.url?.map((url, index) =>
+            <CardActions sx={{ position: 'absolute', bottom: 0 }} disableSpacing>
+                {props.url?.map((url, index) =>
                     <SocialButton key={index} label={url.type} icon={ACTION_ICONS[url.type]?.icon} url={url.href} height="24px" width="24px" color="primary" />
                 )}
-                {/* {props.details.url.map((url, index) =>
+                {/* <Button id="learn" onClick={navigateProject}>Learn More</Button> */}
+                {/* {props.url.map((url, index) =>
                     <Tooltip key={index} title={ACTION_ICONS[url.type]?.tooltip}>
                         {React.forwardRef((props, ref) =>
                             <SocialButton ref={ref} label={url.type} icon={ACTION_ICONS[url.type]?.icon} url={url.href} height="24px" width="24px" />
@@ -186,12 +257,12 @@ function ProjectCard(props) {
                 )} */}
             </CardActions>
         </Card>
-
     )
 }
 
 export {
     Project,
+    ProjectDialog,
     ProjectCard
 };
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,227 +1,34 @@
-import { NavigateBefore, NavigateNext } from '@mui/icons-material'; 
-import { Grid, Box } from '@mui/material';
+import { NavigateBefore, NavigateNext } from '@mui/icons-material';
+import { Box, Grid } from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header.js';
 import NavigateButton from '../components/NavigationButton.js';
-import { ProjectCard } from '../components/Project.js';
-
+import { ProjectCard, ProjectDialog } from '../components/Project.js';
+import PROJECTS from '../Projects-db.js';
 /**
  * Add some sort of sorting algorithm? - move to backend (?)
  */
-const PROJECTS = [
-    {
-        title: "HaxRover",
-        date: "May 2022 - Present",
-        type: "Personal Project - Hardware + Software",
-        shortDescription: "Rover built using a Raspberry Pi Zero W running Ubuntu Desktop 32-bit, motor driver, camera module, and 2 servo motors",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Raspberry Pi', 'Python', 'Android Studio', 'Java', 'Linux', 'BlueZ', 'Bluetooth Low Energy', 'DBus', 'BASH'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/Haxrox/HaxRover"
-            }
-        ]
-    },
-    {
-        title: "Haxdroid",
-        date: "December 2021 - Present",
-        type: "Personal Project - Software",
-        shortDescription: "Discord utility bot written in Node.js hosted on a Microsoft Azure Standard B1 Linux Virtual Machine running Ubuntu Server 16.04-LTS",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Microsoft Azure Virtual Machine', 'Linux', 'BASH', 'Node.js', 'Web Scraping'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/Haxrox/Haxdroid"
-            }
-        ]
-    },
-    {
-        title: "HaxFunctions",
-        date: "December 2021 - Present",
-        type: "Personal Project - Software",
-        shortDescription: "Serverless Backend API for redesigning GitHub Webhook message embeds in Discord",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Microsoft Azure Cloud Functions', 'Node.js'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/Haxrox/HaxFunctions"
-            }
-        ]
-    },
-    {
-        title: "Hello! My Name Is ...",
-        date: "June 2020 - Present",
-        type: "Personal Project - Software",
-        shortDescription: "Android BLE chatting app",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Java', 'Android', 'Bluetooth Low Energy', 'AWS AppSync', 'AWS Amplify Datastore'],
-        url: []
-    },
-    {
-        title: "Personal Website",
-        date: "May 2022",
-        type: "Personal Project - Software",
-        shortDescription: "Portfolio with all my personal projects",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Firebase Hosting', 'React', 'JavaScript', 'GitHub Actions'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/haxrox/haxrox"
-            },
-            {
-                type: "Web",
-                href: "https://haxtech.web.app/"
-            }
-        ]
-    },
-    {
-        title: "The Exploring Rover",
-        date: "March 2022 - April 2022",
-        type: "UBC CPEN 291 - Hardware + Software",
-        shortDescription: "Rover constructed using a Raspberry Pi Zero W running Raspberry Pi OS 32-bit, motor driver, 2 servo motors and camera module",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Raspberry Pi', 'Python', 'Linux', 'WebSockets', 'Node.js', 'Express.js', 'HTML', 'CSS'],
-        url: []
-    },
-    {
-        title: "Fithub",
-        date: "February 2022 - April 2022",
-        type: "UBC Launchpad - Software",
-        shortDescription: "Web application* to help users organize and follow playlists of exercises *NOTE: must accept backend certificate to view videos",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Flutter', 'Node.js', 'Express.js', 'Sequelize', 'PostgreSQL', 'Firebase Hosting', 'GitHub Actions', 'Amazon Elastic Compute (EC2)', 'Amazon Relational Database Service (RDS)'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/haxrox/haxrox"
-            },
-            {
-                type: "Web",
-                href: "https://fithub-launchpad.web.app/#/"
-            },
-            {
-                type: "Backend",
-                href: "https://ec2-54-193-129-230.us-west-1.compute.amazonaws.com:8000/"
-            }
-        ]
-    },
-    {
-        title: "Entertainment Biped Robot",
-        date: "January 2022 - March 2022",
-        type: "UBC CPEN 291 - Hardware + Software",
-        shortDescription: "Biped Robot that dances to a variety of songs using an ItsyBitsy M4 microcontroller, servo motors, colour TFT LCD, piezo buzzer, 3x4 numeric matrix keypad",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['ItsyBitsy', 'CircuitPython'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/Haxrox/HaxRover"
-            }
-        ]
-    },
-    {
-        title: "Sitdown",
-        date: "January 2022",
-        type: "nwHacks Hackathon - Hardware + Software",
-        shortDescription: "Web application to help students find or reserve seats in common study areas",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Firebase Firestore', 'Firebase Hosting', 'Firebase Authentication', 'React', 'Bootstrap', 'JavaScript'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/Haxrox/sitdown"
-            },
-            {
-                type: "Web", 
-                href: "https://nwhacks-sitdown.web.app/"
-            }
-        ]
-    },
-    {
-        title: "Simple RISC Machine",
-        date: "September 2021 - December 2021",
-        type: "UBC CPEN 211 - Hardware",
-        shortDescription: "Reduced Instruction Set Computer (RISC) written in Verilog on an Intel FPGA DE1-SoC",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Assembly', 'Verilog', 'FPGA', 'Quartus', 'ModelSim'],
-        url: []
-    },
-    {
-        title: "Mini Projects",
-        date: "September 2021 - December 2021",
-        type: "UBC CPEN 221 - Software",
-        shortDescription: "Used Java, OOP, algorithm efficiency, concurrency, multithreading, and big data processing concepts to code algorithms for a series of tasks",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Java', 'IntelliJ IDEA'],
-        url: []
-    },
-    {
-        title: "BLE Audio",
-        date: "May 2021",
-        type: "Personal Project - Software",
-        shortDescription: "Android application that uses the Opus Codec to encode and decode audio data to send to another another device using BLE",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Java', 'Android Studio', 'Bluetooth Low Energy', 'Opus Codec'],
-        url: []
-    },
-    {
-        title: "Android Opus Codec Prototype",
-        date: "May 2021",
-        type: "Personal Project - Software",
-        shortDescription: "Android application that uses the Opus Codec to encode and decode audio data",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Java', 'Android Studio', 'Opus Codec'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/Haxrox/Android-OpusCodecPrototype"
-            }
-        ]
-    },
-    {
-        title: "Button Masher",
-        date: "December 2020 - May 2021",
-        type: "Personal Project - Software",
-        shortDescription: "Android application Button Masher game",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['Java', 'Android Studio', 'Firebase Firestore'],
-        url: [
-            {
-                type: "GitHub", 
-                href: "https://github.com/Haxrox/Android-ButtonMasher"
-            }
-        ]
-    },
-    {
-        title: "Claw Retrieval System",
-        date: "February 2021 - March 2021",
-        type: "UBC APSC 101 - Hardware",
-        shortDescription: "Claw manufactured out of household items to pick up multifarious items",
-        longDescription: "Detailed description w/ build instructions, what i learned, etc, etc",
-        techStack: ['C', 'Arduino'],
-        url: [
-            {
-                type: "GitHub",
-                href: "https://github.com/Haxrox/ClawRetrievalSystem"
-            }
-        ]
-    }
-]
 
 function Projects() {
+    const [project, setProject] = useState(undefined);
+    const navigate = useNavigate();
+
     return (
         <Box className="Projects">
             <Header tab="projects"></Header>
-            <Grid container sx={{ m: 2 }} spacing={2} direction = "row">
+            <Grid container sx={{ p: 2, pb: 10 }} spacing={2} direction="row">
                 {PROJECTS.map((project, index) =>
                     <Grid item key={index}>
-                        <ProjectCard details={project} />               
+                        <ProjectCard {...project} navigate={() => {
+                            console.log("Set project");
+                            setProject(project);
+                            navigate(project.title);
+                        }} />               
                     </Grid>
                 )}
             </Grid>
+            <ProjectDialog {...project} />
             <NavigateButton href='/about' left='16px'>
                 <NavigateBefore />
                 About
