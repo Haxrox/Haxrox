f0c4c2fc36b05aee7a48587a5cf2e649425f2889
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-05-26 15:08:57 -0700

    moved themes to diff file

---
+++ src/Theme.js
@@ -0,0 +1,31 @@
+const THEME = {
+    palette: {
+        // mode: 'dark',
+        primary: {
+            light: '#63a4ff',
+            main: '#1976d2',
+            dark: '#004ba0',
+            contrastText: '#ffffff',
+        },
+        secondary: {
+            light: '#7cccff',
+            main: '#389cff',
+            dark: '#006ecb',
+            contrastText: '#000000',
+        },
+        white: {
+            light: '#fff',
+            main: '#fff',
+            dark: '#fff',
+            contrastText: '#000',
+        },
+        black: {
+            light: '#000',
+            main: '#000',
+            dark: '#000',
+            contrastText: '#fff',
+        }
+    }
+};
+
+export default THEME;
