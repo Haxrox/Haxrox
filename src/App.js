0193e2d26a0a952fd20aec1725b79e9afebbe53c
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-05-25 21:36:38 -0700

    moved disclaimer to the bottom

--- src/App.js
+++ src/App.js
@@ -13,44 +13,35 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Box className="App-header" sx={{ bgcolor: 'primary.main' }}>
-        <Grid container spacing={4} direction="column" alignItems="center" justifyContent="center">
-          <Grid item>
-            <Avatar alt="Avatar" src="/images/avatar.jpg" sx={{ width: 128, height: 128 }} />
-          </Grid>
-
-          <Grid item>
-            <Typography variant="h2" fontWeight="bold" textTransform='uppercase' color='primary.contrastText'>
-              Bell Chen (WIP)
-            </Typography>
-          </Grid>
-
-          <Grid item alignItems="center" justifyContent="center">
-            <ButtonGroup variant="contained" aria-label="outlined button group" color='secondary'>
-              {Pages.map(page =>
-                <Button id={page.toLowerCase()} key={page} onClick={navigatePage} sx={{ width: 128 }}>{page}</Button>
-              )}
-            </ButtonGroup>
-          </Grid>
-
-          <Grid item alignItems="center" justifyContent="center">
-            <Grid container spacing={4} direction="row" alignItems="center" justifyContent="center">
-              <Grid item>
-                <SocialButtons />
-              </Grid>
-              {/* <Grid item>
-                <SocialButton label="LinkedIn" url="https://www.linkedin.com/in/bell-chen/" icon = "/icons/LinkedIn.svg"/>
-              </Grid>
-
-              <Grid item>
-                <SocialButton label="GitHub" url="https://github.com/Haxrox" icon = "/icons/GitHub-Mark-120px-plus.png"/>
-              </Grid> */}
-            </Grid>
-          </Grid>
+    <Box className="App-header" sx={{ bgcolor: 'primary.main' }}>
+      <Grid container spacing={4} direction="column" alignItems="center" justifyContent="center">
+        <Grid item>
+          <Avatar alt="Avatar" src="/images/avatar.jpg" sx={{ width: 128, height: 128 }} />
         </Grid>
-      </Box>
-    </div>
+
+        <Grid item>
+          <Typography variant="h2" fontWeight="bold" textTransform='uppercase' color='primary.contrastText'>
+            Bell Chen
+          </Typography>
+        </Grid>
+
+        <Grid item alignItems="center" justifyContent="center">
+          <ButtonGroup variant="contained" aria-label="outlined button group" color='secondary'>
+            {Pages.map(page =>
+              <Button id={page.toLowerCase()} key={page} onClick={navigatePage} sx={{ width: 128 }}>{page}</Button>
+            )}
+          </ButtonGroup>
+        </Grid>
+
+        <Grid item alignItems="center" justifyContent="center">
+          <SocialButtons />
+        </Grid>
+
+      </Grid>
+      <Typography variant="body1" fontWeight="bold" color='primary.contrastText' sx={{ position: 'fixed', bottom: '16px' }}>
+        Portions of this website are still a WIP
+      </Typography>
+    </Box>
   );
 }
 
